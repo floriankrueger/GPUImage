152644f3abd4cd67116d902b9a2117232cd43924
Florian Krüger <florian.krueger@projectserver.org>
2017-08-30 13:09:10 +0200

    :sparkles: enhanced the playground

--- examples/Mac/PlaygroundExample/PlaygroundExample/GPUImagePlayground.playground/section-1.swift
+++ examples/Mac/PlaygroundExample/PlaygroundExample/GPUImagePlayground.playground/section-1.swift
@@ -2,9 +2,26 @@ import Cocoa
 import GPUImage
 
 let sourceImage = NSImage(named: "ChairTest.png")
-let filter = GPUImageSobelEdgeDetectionFilter()
-let outputImage = filter.imageByFilteringImage(sourceImage)
 
-let customFilter = GPUImageFilter(fragmentShaderFromFile: "CustomFilter")
+// a simple edge detection
+let edgeDetectionFilter = GPUImageSobelEdgeDetectionFilter()
+let edgeDetectionImage = edgeDetectionFilter.image(byFilteringImage: sourceImage)
 
-let outputImage2 = customFilter.imageByFilteringImage(sourceImage)
+// pixellate with tweaking parameters 
+// see https://github.com/floriankrueger/GPUImage#image-processing for image processing technique parameters
+// see https://github.com/floriankrueger/GPUImage#visual-effects for visual effect parameters
+let pixellateFilter = GPUImagePixellateFilter()
+pixellateFilter.fractionalWidthOfAPixel = 0.02
+let pixellatedImage = pixellateFilter.image(byFilteringImage: sourceImage)
+
+// custom filter
+let customFilter = GPUImageFilter(fragmentShaderFromFile: "CustomFilter")!
+let customFilteredImage = customFilter.image(byFilteringImage: sourceImage)
+
+// chaining filters
+let chainImageSource = GPUImagePicture(image: sourceImage)! // the input object
+chainImageSource.addTarget(pixellateFilter) // feed the image to pixellation filter
+pixellateFilter.addTarget(customFilter)     // feed the pixellation filter output to the custom filter
+customFilter.useNextFrameForImageCapture()  // capture the next frame that is created (there is only one)
+chainImageSource.processImage()             // run the filter chain
+let chainFilteredImage = customFilter.imageFromCurrentFramebuffer() // the result
