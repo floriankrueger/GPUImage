bc539442b1a9347e9dccb300c6fb935a28dd8dd8
Florian Krüger <florian.krueger@projectserver.org>
2017-08-30 14:55:30 +0200

    :sparkles: display input and output side-by-side

--- examples/Mac/PlaygroundExample/PlaygroundExample/GPUImagePlayground.playground/section-1.swift
+++ examples/Mac/PlaygroundExample/PlaygroundExample/GPUImagePlayground.playground/section-1.swift
@@ -1,7 +1,20 @@
 import Cocoa
 import GPUImage
 
-let sourceImage = #imageLiteral(resourceName: "ChairTest.png")
+// Helpers
+
+func sideBySideImage(input: NSImage, output: NSImage) -> NSImage? {
+    let image = NSImage(size: CGSize(width: input.size.width + output.size.width, height: input.size.height))
+    image.lockFocus()
+    input.draw(in: CGRect(x: 0.0, y: 0.0, width: input.size.width, height: input.size.height))
+    output.draw(in: CGRect(x: input.size.width, y: 0.0, width: output.size.width, height: output.size.height))
+    image.unlockFocus()
+    return image
+}
+
+// Images
+
+let imgChair = #imageLiteral(resourceName: "ChairTest.png")
 let imgBeauty = #imageLiteral(resourceName: "DTS_Beauty.jpg")
 let imgBMX = #imageLiteral(resourceName: "DTS_BMX.jpg")
 let imgBody = #imageLiteral(resourceName: "DTS_Body.jpg")
@@ -11,26 +24,30 @@ let imgKinck = #imageLiteral(resourceName: "DTS_Kinckerbocker.jpg")
 
 // a simple edge detection
 let edgeDetectionFilter = GPUImageSobelEdgeDetectionFilter()
-let edgeDetectionImage = edgeDetectionFilter.image(byFilteringImage: sourceImage)
+let edgeDetectionImage = edgeDetectionFilter.image(byFilteringImage: imgChair)
+sideBySideImage(input: imgChair, output: edgeDetectionImage!)
 
-// pixellate with tweaking parameters 
+// pixellate with tweaking parameters
 // see https://github.com/floriankrueger/GPUImage#image-processing for image processing technique parameters
 // see https://github.com/floriankrueger/GPUImage#visual-effects for visual effect parameters
 let pixellateFilter = GPUImagePixellateFilter()
 pixellateFilter.fractionalWidthOfAPixel = 0.02
-let pixellatedImage = pixellateFilter.image(byFilteringImage: sourceImage)
+let pixellatedImage = pixellateFilter.image(byFilteringImage: imgChair)
+sideBySideImage(input: imgChair, output: pixellatedImage!)
 
 // custom filter
 let customFilter = GPUImageFilter(fragmentShaderFromFile: "CustomFilter")!
-let customFilteredImage = customFilter.image(byFilteringImage: sourceImage)
+let customFilteredImage = customFilter.image(byFilteringImage: imgChair)
+sideBySideImage(input: imgChair, output: customFilteredImage!)
 
 // chaining filters
-let chainImageSource = GPUImagePicture(image: sourceImage)! // the input object
+let chainImageSource = GPUImagePicture(image: imgChair)! // the input object
 chainImageSource.addTarget(pixellateFilter) // feed the image to pixellation filter
 pixellateFilter.addTarget(customFilter)     // feed the pixellation filter output to the custom filter
 customFilter.useNextFrameForImageCapture()  // capture the next frame that is created (there is only one)
 chainImageSource.processImage()             // run the filter chain
 let chainFilteredImage = customFilter.imageFromCurrentFramebuffer() // the result
+sideBySideImage(input: imgChair, output: chainFilteredImage!)
 
 // multi-image
 
@@ -44,9 +61,9 @@ let filtered = filter(images: images) { image in
     return filter.image(byFilteringImage: image)
 }
 
-filtered[0]
-filtered[1]
-filtered[2]
-filtered[3]
-filtered[4]
-filtered[5]
+sideBySideImage(input: images[0], output: filtered[0])
+sideBySideImage(input: images[1], output: filtered[1])
+sideBySideImage(input: images[2], output: filtered[2])
+sideBySideImage(input: images[3], output: filtered[3])
+sideBySideImage(input: images[4], output: filtered[4])
+sideBySideImage(input: images[5], output: filtered[5])
