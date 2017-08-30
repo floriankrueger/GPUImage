95a90e68057e36eef6b3ecda33472b40fbb11d12
Florian Krüger <florian.krueger@projectserver.org>
2017-08-30 13:28:26 +0200

    :sparkles: add more examples

--- examples/Mac/PlaygroundExample/PlaygroundExample/GPUImagePlayground.playground/section-1.swift
+++ examples/Mac/PlaygroundExample/PlaygroundExample/GPUImagePlayground.playground/section-1.swift
@@ -1,7 +1,13 @@
 import Cocoa
 import GPUImage
 
-let sourceImage = NSImage(named: "ChairTest.png")
+let sourceImage = #imageLiteral(resourceName: "ChairTest.png")
+let imgBeauty = #imageLiteral(resourceName: "DTS_Beauty.jpg")
+let imgBMX = #imageLiteral(resourceName: "DTS_BMX.jpg")
+let imgBody = #imageLiteral(resourceName: "DTS_Body.jpg")
+let imgCuba = #imageLiteral(resourceName: "DTS_Cuba.jpg")
+let imgHotCold = #imageLiteral(resourceName: "DTS_HotCold.jpg")
+let imgKinck = #imageLiteral(resourceName: "DTS_Kinckerbocker.jpg")
 
 // a simple edge detection
 let edgeDetectionFilter = GPUImageSobelEdgeDetectionFilter()
@@ -25,3 +31,22 @@ pixellateFilter.addTarget(customFilter)     // feed the pixellation filter outpu
 customFilter.useNextFrameForImageCapture()  // capture the next frame that is created (there is only one)
 chainImageSource.processImage()             // run the filter chain
 let chainFilteredImage = customFilter.imageFromCurrentFramebuffer() // the result
+
+// multi-image
+
+func filter(images: [NSImage], using filter: (NSImage) -> NSImage) -> [NSImage] {
+    return images.map { filter($0) }
+}
+
+let images = [imgBeauty, imgBMX, imgBody, imgCuba, imgHotCold, imgKinck]
+let filtered = filter(images: images) { image in
+    let filter = GPUImageGrayscaleFilter()
+    return filter.image(byFilteringImage: image)
+}
+
+filtered[0]
+filtered[1]
+filtered[2]
+filtered[3]
+filtered[4]
+filtered[5]
